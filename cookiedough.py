01bf71b9f555842ded902b5e6511695fc2ec0c0a
BlueAgent <BlueAgent@users.noreply.github.com>
2019-08-20 03:26:44 +0800

    Fixed logging of command args and timestamp

--- cookiedough.py
+++ cookiedough.py
@@ -3,7 +3,7 @@ import sys
 
 from discord.ext import commands
 
-logging.basicConfig(level=logging.INFO, format='%(message)s %(asctime)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s][%(levelname)s] %(message)s', datefmt='%Y-%m-%d_%H:%M:%S')
 
 description = '''A cookie loving Discord bot'''
 bot = commands.Bot(command_prefix='.', description=description)
@@ -11,13 +11,18 @@ bot = commands.Bot(command_prefix='.', description=description)
 
 @bot.event
 async def on_ready():
-    print(f'Cookie Dough is logged in as {bot.user}')
+    logging.info(f'Cookie Dough is logged in as {bot.user}')
 
 
 @bot.event
 async def on_command_completion(ctx):
-    """Logs all commands that completed without error"""
-    logging.info(f'{ctx.author} used {ctx.command} with args={ctx.args} and kwargs={ctx.kwargs}')
+    """Logs all commands that completed successfully"""
+    msg = f'{ctx.author} used {ctx.command}'
+    if len(ctx.args) > 1:
+        msg = msg + f' args={ctx.args[1:]}'
+    if len(ctx.kwargs) > 0:
+        msg = msg + f' kwargs={ctx.kwargs}'
+    logging.info(msg)
 
 
 @bot.command()
