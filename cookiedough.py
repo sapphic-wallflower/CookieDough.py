448994e2d050c3bc91961ae0138db218f5b02825
sighofrelief <deepsighofrelief@gmail.com>
2019-10-27 00:58:26 -0500

    Enabling Automod, Disabling Testing

--- cookiedough.py
+++ cookiedough.py
@@ -11,7 +11,9 @@ bot = commands.Bot(command_prefix='.', description=description, case_insensitive
 
 bot.load_extension("cogs.main")
 bot.load_extension("cogs.admin")
+bot.load_extension('cogs.automod')
 bot.load_extension("cogs.stickers")
 bot.load_extension("cogs.fun")
+# bot.load_extension("cogs.testing")
 
 bot.run(sys.argv[1])
