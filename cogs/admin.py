479c24294f77d3743778000316eb5c2b85a06b18
sighofrelief <deepsighofrelief@gmail.com>
2019-11-25 06:11:03 -0600

    Added .say command, .pinpurge command
    .say allows those with the manage guild permission to have cookie dough say anything they want in any channel they want, so long as cookie dough has permission to talk in that channel.
    
    Pinpurge unpins n most recent pinned messages in a channel.

--- cogs/admin.py
+++ cogs/admin.py
@@ -18,7 +18,7 @@ class Admin(commands.Cog):
         await reply.delete(delay=1)
 
     @commands.command()
-    @commands.has_permissions()
+    @commands.has_permissions(manage_guild=True)
     async def status(self, bot):
         """Set the bot's playing status"""
         command_prefix = self.bot.command_prefix
@@ -29,6 +29,44 @@ class Admin(commands.Cog):
             message = bot.message.system_content.replace(f'{command_prefix}status ', '')
             await self.bot.change_presence(activity=discord.Game(f"{command_prefix}help | {message}"))
             await bot.channel.send(f"Set status to \"Playing **{command_prefix}help | {message}**\"!")
+
+    @commands.command()
+    @commands.has_permissions(manage_guild=True)
+    async def say(self, bot):
+        """Have Cookie Dough repeat a phrase in a channel"""
+        command_prefix = self.bot.command_prefix
+        target_channel_id = bot.message.raw_channel_mentions[0]
+        if bot.message.content.startswith(f'{command_prefix}say <#{target_channel_id}> ') is False:
+            await bot.channel.send(f"use `{command_prefix}say [target_channel] [message]` to have me say the contents of [message] in whatever channel you pick!")
+            return
+        msg = bot.message.content.replace(f'{command_prefix}say <#{target_channel_id}> ', '')
+        for channel in bot.message.guild.channels:
+            if channel.id == target_channel_id:
+                await channel.send(f"{msg}")
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    @commands.bot_has_permissions(manage_messages=True, read_message_history=True)
+    async def pinpurge(self, bot, n: int):
+        """Delete all pins in a channel, except ones sent from cookiedough"""
+        command_prefix = self.bot.command_prefix
+        pins = await bot.message.channel.pins()
+        count = 0
+        if n <= 0 or n > 49:
+            await bot.channel.send(f'how many pins did you want me to unpin in this channel? (`{command_prefix}pinpurge [number]`)')
+            return
+        timewarning = await bot.channel.send(f'unpinning {n} pins in this channel, this may take awhile...')
+        for pin in pins:
+            if pin.author.id == bot.me.id:
+                continue
+            if count > n:
+                break
+            await pin.unpin()
+            count = count+1
+        await bot.channel.send('finished unpinning!', delete_after=8)
+        await timewarning.delete()
+        await bot.message.delete()
+
 
 def setup(bot):
     bot.add_cog(Admin(bot))
