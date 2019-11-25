64cb4588f9f29ccae8e9faa86ce434966aacee28
sighofrelief <deepsighofrelief@gmail.com>
2019-11-25 01:03:05 -0600

    Created Misc.py
    Movwd GBJ meetup map to misc.py.
    Fixed a bug in admin.py that caused the status command to update incorrectly

---
+++ cogs/misc.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+class misc(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command()
+    async def meetupmap(self, ctx: commands.context.Context):
+        """Link the server\'s meetup map"""
+        thumbnail = discord.File('stickers_unused/meetup_map_thumbnail.png')
+        await ctx.send(
+            content='**Gay Baby Jail Meetup Map:** https://tinyurl.com/yd9bcf7u\nIf you wish to add a marker, contact Sigh with your Country, City (optional), State/Providence, Description (optional), Color (optional) and Photos (optional).\nNote that if you change your discord username, people will not be able to find you from the map, and that the thumbnail may be out of date',
+            file=thumbnail)
+
+def setup(bot):
+    bot.add_cog(misc(bot))
