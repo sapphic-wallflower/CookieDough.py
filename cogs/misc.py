3f7c4cd9a480f5ce2d43faf9c0bac7bb26eb9c76
sighofrelief <deepsighofrelief@gmail.com>
2019-12-31 03:06:27 -0600

    bugfix, added affiliate commands
    removed diaperblue and diaperpink from emoji, they were already in clothes
    
    renamed diaperlist to diaperidentifier, created a conflict with the new command
    
    added Amazon Affiliate commands

--- cogs/misc.py
+++ cogs/misc.py
@@ -13,6 +13,33 @@ class misc(commands.Cog):
             content='**Gay Baby Jail Meetup Map:** https://tinyurl.com/yd9bcf7u\nIf you wish to add a marker, contact Sigh with your Country, City (optional), State/Providence, Description (optional), Color (optional) and Photos (optional).\nNote that if you change your discord username, people will not be able to find you from the map, and that the thumbnail may be out of date',
             file=thumbnail)
 
+    @commands.command(aliases=["diaplist"])
+    async def diaperlist(self, ctx: commands.context.Context):
+        """Amazon Affiliate diaper list"""
+        await ctx.send('**Every ABDL Diaper on Amazon:** https://amzn.to/2tueosG \nNote that this is an affiliate link, so Gay Baby Jail may get kickback on applicable items.')
 
+    @commands.command(aliases=["plasticlist"])
+    async def clothlist(self, ctx: commands.context.Context):
+        """Amazon Affiliate cloth and plastic diaper list"""
+        await ctx.send(
+            '**Plastic Pants and Cloth Diapers:** https://amzn.to/2PDQKCE \nNote that this is an affiliate link, so Gay Baby Jail may get kickback on applicable items.')
+
+    @commands.command(aliases=["pacilist", "accessorieslist"])
+    async def accessorylist(self, ctx: commands.context.Context):
+        """Amazon Affiliate accessory list"""
+        await ctx.send(
+            '**A ton of ABDL accessories:** https://amzn.to/35MqXOA \nNote that this is an affiliate link, so Gay Baby Jail may get kickback on applicable items.')
+
+    @commands.command(aliases=["onesielist", "dresseslist", "dresslist"])
+    async def outfitlist(self, ctx: commands.context.Context):
+        """Amazon Affiliate Onesies, Outfits, and Dresses"""
+        await ctx.send(
+            '**Onesies, Outfits, and Dresses:** https://amzn.to/2Q6RIX0 \nNote that this is an affiliate link, so Gay Baby Jail may get kickback on applicable items.')
+
+    @commands.command(aliases=["stuffylist"])
+    async def stuffielist(self, ctx: commands.context.Context):
+        """Amazon Affiliate Stuffie list"""
+        await ctx.send(
+            '**Stuffie list:** https://amzn.to/2Q7oac9 \nNote that this is an affiliate link, so Gay Baby Jail may get kickback on applicable items.')
 def setup(bot):
     bot.add_cog(misc(bot))
