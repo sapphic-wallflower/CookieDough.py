82b10ea60fc9cc65fd40430da0fdd4a05ee3c90f
sighofrelief <deepsighofrelief@gmail.com>
2020-06-25 22:37:40 -0500

    updated stickers, updated requirements, etc
    added a few stickers, optimized others, unembeded the tiny.url link for the meetupmap (as it formly didn't embed)

--- cogs/misc.py
+++ cogs/misc.py
@@ -16,7 +16,7 @@ class misc(commands.Cog):
         """Link the server\'s meetup map"""
         thumbnail = discord.File('stickers_unused/meetup_map_thumbnail.png')
         await ctx.send(
-            content='**Gay Baby Jail Meetup Map:** https://tinyurl.com/yd9bcf7u\nIf you wish to add a marker, contact Sigh with your Country, City (optional), State/Providence, Description (optional), Color (optional) and Photos (optional).\nNote that if you change your discord username, people will not be able to find you from the map, and that the thumbnail may be out of date',
+            content='**Gay Baby Jail Meetup Map:** <https://tinyurl.com/yd9bcf7u>\nIf you wish to add a marker, contact Sigh with your Country, City (optional), State/Providence, Description (optional), Color (optional) and Photos (optional).\nNote that if you change your discord username, people will not be able to find you from the map, and that the thumbnail may be out of date',
             file=thumbnail)
 
     @commands.command(aliases=["donations", "support"])
