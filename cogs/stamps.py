095791dbc6c3a3b20ef383dc8689de44c6751e2f
Gwenily <gwenily@protonmail.com>
2022-07-18 00:29:54 -0500

    Rename stickers to stamps, edit command, update requirements
    added the .edit command
    renamed "stickers" to "stamps" to avoid confusion with the vanilla discord feature
    updated requirements.txt to be as up to date as possible while keeping co-dependencies working

--- cogs/stamps.py
+++ cogs/stamps.py
@@ -21,19 +21,19 @@ def snake_to_title(name):
     return " ".join(word.capitalize() for word in name.split("_"))
 
 
-class Stickers(commands.Cog):
+class stamps(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.category_sticker_names = dict()
+        self.category_stamp_names = dict()
         self.category_names = []
         self._walk_root()
 
     def _walk_root(self):
-        """Registers sticker commands for each image in each category
+        """Registers stamp commands for each image in each category
         category can be configured with .json file of the same name
-        prefix will append the string to all the stickers inside
+        prefix will append the string to all the stamps inside
         """
-        root = Path('./stickers')
+        root = Path('./stamps')
         root.mkdir(exist_ok=True)
 
         for category_path in root.iterdir():
@@ -42,7 +42,7 @@ class Stickers(commands.Cog):
                     'hidden': True,
                     'name': snake_to_camel(category_path.stem),
                     'prefix': '',
-                    'message': f'{snake_to_title(category_path.stem)} stickers\n```{{}}```',
+                    'message': f'{snake_to_title(category_path.stem)} stamps\n```{{}}```',
                     'file': None
                 }
 
@@ -65,11 +65,11 @@ class Stickers(commands.Cog):
                 # config_str = f' {category_config}' if category_config else ''
                 # print(f'{category_path}{config_str}')
 
-                sticker_names = self._walk_category(category_path, prefix=category_config['prefix'])
+                stamp_names = self._walk_category(category_path, prefix=category_config['prefix'])
 
-                self.category_sticker_names[category_name] = sticker_names
+                self.category_stamp_names[category_name] = stamp_names
 
-                category_config['sticker_names'] = sticker_names
+                category_config['stamp_names'] = stamp_names
 
                 cmd = self._category_command(category_config)
                 self.bot.add_command(cmd)
@@ -80,9 +80,9 @@ class Stickers(commands.Cog):
         name = config['name']
         file = config['file']
         message = config['message']
-        sticker_names = config['sticker_names']
+        stamp_names = config['stamp_names']
 
-        formatted_message = message.format(" ".join(sticker_names)).strip()
+        formatted_message = message.format(" ".join(stamp_names)).strip()
 
         async def callback(cog, ctx):
             final_file = None if file is None else discord.File(file)
@@ -92,7 +92,7 @@ class Stickers(commands.Cog):
             callback,
             hidden=hidden,  # Don't show category commands in the usual help
             name=name,
-            help=f'Info about {name} sticker category'
+            help=f'Info about {name} stamp category'
         )
 
         cmd.cog = self
@@ -100,50 +100,50 @@ class Stickers(commands.Cog):
         return cmd
 
     def _walk_category(self, category_path: Path, prefix=""):
-        """Registers commands for all the stickers inside
-        sticker can be configured with .json file
+        """Registers commands for all the stamps inside
+        stamp can be configured with .json file
         returns the name of all the commands
         """
-        sticker_names = []
+        stamp_names = []
 
-        sticker_dict = dict()
+        stamp_dict = dict()
 
         # Collect image and config for each image
-        for sticker_path in category_path.iterdir():
-            if sticker_path.is_file() and sticker_path.suffix in IMAGE_AND_JSON_SUFFIXES:
-                key = sticker_path.stem
-                if key not in sticker_dict:
-                    # Sticker Config Defaults
-                    sticker_dict[key] = {
+        for stamp_path in category_path.iterdir():
+            if stamp_path.is_file() and stamp_path.suffix in IMAGE_AND_JSON_SUFFIXES:
+                key = stamp_path.stem
+                if key not in stamp_dict:
+                    # stamp Config Defaults
+                    stamp_dict[key] = {
                         'hidden': True,
-                        'name': f'{prefix}{snake_to_camel(sticker_path.stem)}',
+                        'name': f'{prefix}{snake_to_camel(stamp_path.stem)}',
                         'aliases': [],
                         'message': '',
                         'file': None,
                     }
-                sticker_config = sticker_dict[key]
+                stamp_config = stamp_dict[key]
 
-                if sticker_path.suffix in IMAGE_SUFFIXES:
-                    sticker_config['file'] = sticker_path
+                if stamp_path.suffix in IMAGE_SUFFIXES:
+                    stamp_config['file'] = stamp_path
                 else:
                     try:
-                        with sticker_path.with_suffix('.json').open() as fp:
-                            sticker_config.update(json.load(fp))
+                        with stamp_path.with_suffix('.json').open() as fp:
+                            stamp_config.update(json.load(fp))
                     except IOError as e:
                         pass  # Ignore if not found
 
         # Create and add a dynamic command for each config object
-        for sticker_config in sticker_dict.values():
-            sticker_names.append(sticker_config['name'])
+        for stamp_config in stamp_dict.values():
+            stamp_names.append(stamp_config['name'])
 
-            # print(f'{sticker_path} {sticker_config}')
-            cmd = self._sticker_command(sticker_config)
+            # print(f'{stamp_path} {stamp_config}')
+            cmd = self._stamp_command(stamp_config)
             self.bot.add_command(cmd)
 
-        return sticker_names
+        return stamp_names
 
-    def _sticker_command(self, config):
-        """Generate a dynamic sticker command object"""
+    def _stamp_command(self, config):
+        """Generate a dynamic stamp command object"""
         name = config['name']
         file = config['file']
         aliases = config['aliases']
@@ -151,7 +151,7 @@ class Stickers(commands.Cog):
         hidden = config['hidden']
 
         async def callback(cog, ctx):
-            log = logging.getLogger("cogs.stickers")
+            log = logging.getLogger("cogs.stamps")
             final_file = None if file is None else discord.File(file)
             channel_name = ctx.channel.name
             wh_info_found = None
@@ -174,16 +174,16 @@ class Stickers(commands.Cog):
                 await webhook.send(avatar_url=f'{ctx.author.avatar_url}', username=ctx.author.display_name, content=message, file=final_file)
 
 
-        cmd = commands.Command(callback, hidden=hidden, name=name, aliases=aliases, help=f'Send {name} sticker')
+        cmd = commands.Command(callback, hidden=hidden, name=name, aliases=aliases, help=f'Send {name} stamp')
         cmd.cog = self
         return cmd
 
     @commands.command()
-    async def stickers(self, ctx):
-        """Prints out a list of sticker categories"""
+    async def stamps(self, ctx):
+        """Prints out a list of stamp categories"""
         categories = " ".join(self.category_names)
-        await ctx.send(f'Here\'s a list of our sticker packs!\nType `{self.bot.command_prefix}[pack-name]` to see a list of the stickers inside of that pack.```{categories}``` Also, you can add stickers in the `!shop`!'.strip())
+        await ctx.send(f'Here\'s a list of our stamp packs!\nType `{self.bot.command_prefix}[pack-name]` to see a list of the stamps inside of that pack.```{categories}``` Also, you can add stamps in the `!shop`!'.strip())
 
 
 def setup(bot):
-    bot.add_cog(Stickers(bot))
+    bot.add_cog(stamps(bot))
--- cookiedough.py
+++ cookiedough.py
@@ -11,9 +11,9 @@ bot = commands.Bot(command_prefix='.', description=description, case_insensitive
 bot.load_extension("cogs.admin")
 bot.load_extension("cogs.fun")
 bot.load_extension("cogs.main")
-bot.load_extension("cogs.misc")
 bot.load_extension("cogs.pinboard")
-bot.load_extension("cogs.stickers")
+bot.load_extension("cogs.stamps")
+# bot.load_extension("cogs.misc")
 # bot.load_extension("cogs.testing")
 
 bot.run(sys.argv[1])
